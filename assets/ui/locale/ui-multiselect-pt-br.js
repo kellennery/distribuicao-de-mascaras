cd06a076c810e5d8947737ff4877dbd5386119f9
kellennery <113566941+kellennery@users.noreply.github.com>
2024-11-23 21:27:48 -0300

    Add files via upload

---
+++ assets/ui/locale/ui-multiselect-pt-br.js
@@ -0,0 +1,11 @@
+/**
+ * Localization strings for the UI Multiselect widget
+ *
+ * @locale pt-BR, pt-BR
+ */
+
+$.extend($.ui.multiselect.locale, {
+	addAll:'+ todos',
+	removeAll:'- todos',
+	itemsCount:'items selecionados'
+});
